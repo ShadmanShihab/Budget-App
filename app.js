4299320238963ce31d75addd6b75dc12eebd33d3
shadman_shihab <shadmanshihab0@gmail.com>
2020-10-19 13:58:43 +0600

    test module

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 var budgetController = (function(){
-    
+    var x = 23;
+
+    var add = function(a){
+        return x + a;
+    }
+
+    return {
+        publicTest : function(b){
+            //console.log(add(b))
+            return add(b);
+        }
+    }
 })();
 
 var UIController = (function(){
@@ -8,5 +19,11 @@ var UIController = (function(){
 
 var controller = (function(budgetCtrl,  UICtrl){
 
-    
+    var z = budgetCtrl.publicTest(5);
+
+    return {
+        anotherPublic: function() {
+            console.log(z);
+        }
+    }
 })(budgetController, UIController);
