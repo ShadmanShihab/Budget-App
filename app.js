ff5ddd1fec19a6ad1e74cafb06e6fc368f0b70ed
shadman_shihab <shadmanshihab0@gmail.com>
2020-10-19 13:56:29 +0600

    setting modules

---
+++ app.js
@@ -0,0 +1,12 @@
+var budgetController = (function(){
+    
+})();
+
+var UIController = (function(){
+    //code here
+})();
+
+var controller = (function(budgetCtrl,  UICtrl){
+
+    
+})(budgetController, UIController);
